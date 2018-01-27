5785c880fd0c9b4affd56c1a3b821cbcaaf58647
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-01-27 15:33:36 +0100

    fix request to epci data

--- capeb/public/js/map.js
+++ capeb/public/js/map.js
@@ -1,22 +1,40 @@
-var body = d3.select("body");
-var svg=body.append("svg");
+"use strict";
+
+
+var body = d3.select("#map");
+var svg = body.append("svg");
 svg.attr("width", 600).attr("height", 600);
 var projection = d3.geoConicConformal()
     .center([2.454071, 46.279229])
-    .scale(10000)
-    .translate([800,500]);
+    .scale(12000)
+    .translate([1000, 500]);
 var path = d3.geoPath(projection);
 
 path.projection(projection);
-d3.json("data/contours-epci-2017.geojson", function(err, geoJSON) {
-    var map=svg.selectAll("path").data(geoJSON.features);
+d3.json("data/contours-epci-2017.geojson", function (err, geoJSON) {
+    var map = svg.selectAll("path").data(geoJSON.features);
     map.enter()
         .append("path")
-            .attr("fill","white")
-            .attr("stroke","black")
-            .attr("d", path)
-            .attr("siren_epci",function (d) { return d.properties.siren_epci; })
-            .on("click", function(d) {console.log(d.properties.nom_comple);})
+        .attr("fill", "white")
+        .attr("stroke", "black")
+        .attr("d", path)
+        .on("click", function (d) {
+            console.log(d.properties.siren_epci);
+            fetch("/capeb/data/" + d.properties.siren_epci + "/stats")
+                .then(function (value) {
+                    return value.json();
+                })
+                .catch(function (error) {
+                    console.log("error");
+                    console.log(error);
+                    return {};
+                })
+                .then(function(json){
+                    console.log(json);
+                })
+        })
         .append("svg:title")
-            .text(function(d) {return d.properties.nom_comple});
+        .text(function (d) {
+            return d.properties.nom_comple
+        });
 });
