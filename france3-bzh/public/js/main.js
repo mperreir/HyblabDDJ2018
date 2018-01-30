662cec377f5b6943d66bf8cce7d6b42784185604
clementian067@gmail.com <clementian067@gmail.com>
2018-01-30 18:12:24 +0100

    change nam files

---
+++ france3-bzh/public/js/main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// No need for window.onload event here since we are using the def attribute
+// when loading our scripts
+
+
+//---------------------
+// Initialise le fullpage
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
+
+
+
+// Popmotion
+// const { easing, tween, styler } = window.popmotion;
+//
+// const divStyler = styler(document.querySelector('#train'));
+// var myTarget=document.querySelectorAll('#train');
+//
+//
+// tween({
+//   from: 200,
+//   to: { x: 800, y:500},
+//   duration: 1000,
+//   // ease: easing.backOut,
+//   // flip: 1,
+//   // elapsed: 500,
+//   // loop: 5,
+//   // yoyo: 5
+// }).start(divStyler.set);
+
+
+// chartjs
+// Load a dummy json file using the fetch API
+fetch('data/nb_V_A_T.json')
+    // this promise will be fulfilled when the json fill will be
+    .then(function (response){
+        // if we could load the resource, parse it
+        if( response.ok )
+            return response.json();
+        else // if not, send some error message as JSON data
+            return {data: "JSON file not found"};
+
+    })
+    // in case of invalid JSON (parse error) send some error message as JSON data
+    .catch( function (error){
+        return {data: "Invalid JSON"};
+    })
+    // this promise will be fulfilled when the json will be parsed
+    .then(function (json) {
+        console.log(json);
+    });
