756369840c4dc700467b8778b94f0c15e9795947
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-02-01 16:44:41 +0100

    add json formatting for bubble graph

--- capeb/api/api.js
+++ capeb/api/api.js
@@ -9,17 +9,17 @@ var path = require('path');
 var fs = require('fs');
 
 var data_capeb = path.join(__dirname,'./data/CAPEBPaysDelaLoire_2014-2017.csv')
-var stat_files = ['Activité2017.csv', 
-				  'Développement_durable2016.csv', 
-				  'Marchés_publics2017.csv', 
+var stat_files = ['Activité2017.csv',
+				  'Développement_durable2016.csv',
+				  'Marchés_publics2017.csv',
 				  'Zone_intervention2017.csv',
 				  'Contrats_2014-2017.csv'
 				  ].map(f => path.join(__dirname,"./data/stats/".concat(f)))
 
 var stats_json = {}
-var critere = ['Activite', 
-  			   'Developpement_durable', 
-  			   'Marches_publics', 
+var critere = ['Activite',
+  			   'Developpement_durable',
+  			   'Marches_publics',
   			   'Zone_intervention',
   			   'Contrats',
   			  ]
@@ -41,7 +41,7 @@ app.get('/:epci/stats', (req, res) => {
 			}
 			else{
 				var cells = line.split(',')
-				
+
 				if(cells[0] == req.params.epci){
 					if(idf >= 4){
 						cells.slice(1).map((val, j) => {
@@ -60,4 +60,30 @@ app.get('/:epci/stats', (req, res) => {
 	res.charset = 'utf8';
 	res.json(stats_json);
 })
+
+app.get('/:epci/bubble', function(req, res){
+    var bubblecsv = path.join(__dirname,'./data/stats/BubbleChart.csv');
+    var json = {};
+    crt_arr = []
+    var data = fs.readFileSync(bubblecsv, 'utf8');
+    data.split(/\r\n|\n/).forEach(function (line, id) {
+        if(id == 0){
+            keys = line.split(";");
+            json['labels'] = keys.slice(1);
+            json['values'] =  [];
+        }
+        else{
+            var cells = line.split(';');
+            if(cells[0] == req.params.epci){
+                json['values'].push(cells.slice(1));
+            }
+        }
+});
+
+res.charset = 'utf8';
+res.json(json);
+});
+
+
+
 module.exports = app;
