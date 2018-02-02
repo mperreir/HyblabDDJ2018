915172a934f1838f6a3cd7e38cce0b50c6718df6
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-02-02 15:05:30 +0100

    add bubble chart and website structure

--- capeb/public/js/map.js
+++ capeb/public/js/map.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var page2 = d3.select("#page2");
+var page2 = d3.select(".map-pdl");
 var svg = page2.append("svg");
 svg.attr("width", 1000).attr("height", 1000);
 var projection = d3.geoConicConformal()
@@ -20,11 +20,12 @@ $ (document).ready(function(){
             .attr("stroke", "black")
             .attr("d", path)
             .on("click", function (d) {
-                document.getElementById("page3").style.display = "block";
-                $('html,body').animate({
-                        scrollTop: $("#page3").offset().top},
-                    'slow');
+                // transition
+                var options = {target_id:"screen2", animation_command:"left"};
+                MultiScreen.switch_screens(options);
+                console.log(MultiScreen.get_current_screen());
 
+                document.getElementById("nom_epci").innerText = d.properties.nom_comple;
                 // sunburst and basic charts
                 fetch("/capeb/data/" + d.properties.siren_epci + "/stats")
                     .then(function (value) {
@@ -36,7 +37,6 @@ $ (document).ready(function(){
                         return {};
                     })
                     .then(function(json){
-                        console.log(d.properties.siren_epci);
                         flush();
                         drawBarChart(json.Activite, "Répartion des activités entre 2014 et 2017");
                         drawPieChart(json.Developpement_durable, "Dévoloppement durable en 2016");
@@ -56,28 +56,27 @@ $ (document).ready(function(){
                         return {};
                     })
                     .then(function(json){
-                        drawBubbleChart(json);
+                        // drawBubbleChart(json);
                     });
             })
             .on("mouseover", function(d) {
                 d3.select(this).transition()
                     .delay("50")
                     .duration("200")
-                    .style("fill", "blue");
-                d3.select("#bigText").text(d.properties.nom_comple);
+                    .style("fill", "#CCE748")
+                    .style("cursor", "pointer");
+                d3.select("#titre-epci").text(d.properties.nom_comple);
             })
             .on("mouseout", function() {
                 d3.select(this).transition()
                     .delay("50")
                     .duration("200")
                     .style("fill", "white");
-                d3.select("#bigText").text('');
+                d3.select("#titre-epci").text('');
             })
-            .append("svg:title")
-            .text(function (d) {
-                return d.properties.nom_comple
-            });
 
     });
 
 });
+
+
