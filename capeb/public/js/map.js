d2d6a7f18e7ac8b48936a2d980b25385c291408a
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-02-03 22:17:03 +0100

    added mini dataviz for emploi

--- capeb/public/js/map.js
+++ capeb/public/js/map.js
@@ -66,16 +66,16 @@ $ (document).ready(function(){
 function miniStats(regionStats, d) {
 
     var colorsForRegion = [
-    "rgba(240,101,85,1)",
-    "rgba(244,133,64,1)",
-    "rgba(248,165,43,1)",
-    "rgba(251,157,21,1)",
-    "rgba(255,229,0,1)",
-    "rgba(225,223,36,1)",
-    "rgba(195,216,76,1)",
-    "rgba(165,210,113,1)",
-    "rgba(135,203,151,1)",
-    "rgba(105,197,185,1)"
+        "rgba(240,101,85,1)",
+        "rgba(244,133,64,1)",
+        "rgba(248,165,43,1)",
+        "rgba(251,157,21,1)",
+        "rgba(255,229,0,1)",
+        "rgba(225,223,36,1)",
+        "rgba(195,216,76,1)",
+        "rgba(165,210,113,1)",
+        "rgba(135,203,151,1)",
+        "rgba(105,197,185,1)"
     ];
 
     //1 : conjoncture / number / multibubble
@@ -88,7 +88,7 @@ function miniStats(regionStats, d) {
             console.log(error);
             return {};
         })
-        .then(function(json){
+        .then(function (json) {
             var dataFrame = document.getElementsByClassName("info-conjoncture")[0];
             dataFrame.getElementsByClassName("donnee")[0].innerHTML = parseFloat(json.values[0][0]).toFixed(1);
             dataFrame.style.backgroundColor = colorsForRegion[matchColor(json.values[0][0], 2.87, 3.78, 0.182, 0.244, false)];
@@ -105,17 +105,17 @@ function miniStats(regionStats, d) {
             console.log(error);
             return {};
         })
-        .then(function(json){
+        .then(function (json) {
             var dataFrame = document.getElementsByClassName("info-investissement")[0];
-            dataFrame.getElementsByClassName("donnee")[0].innerHTML = (json.values[3][1]*100).toFixed(1);; // only 2017 and percentage of yes
+            dataFrame.getElementsByClassName("donnee")[0].innerHTML = (json.values[3][1] * 100).toFixed(1);
+            ; // only 2017 and percentage of yes
             dataFrame.style.backgroundColor = colorsForRegion[matchColor(json.values[3][1], 0, 0.15, 0.03, 0.17, false)];
-            console.log(matchColor(json.values[3][1], 0, 0.15, 0.3, 0.17, false))
         });
 
 
-	//3 & 8 pour eviter re faire un fetch
-		fetch("/capeb/data/" + d.properties.siren_epci + "/stats")
-		.then(function (value) {
+    //3 & 8 pour eviter re faire un fetch
+    fetch("/capeb/data/" + d.properties.siren_epci + "/stats")
+        .then(function (value) {
             return value.json();
         })
         .catch(function (error) {
@@ -123,31 +123,37 @@ function miniStats(regionStats, d) {
             console.log(error);
             return {};
         })
-        .then(function(stats){
-           var contrats = stats.Contrats.values.slice(1)
-           var stat = contrats.map((val, id) => {return {"name": stats.Contrats.labels[id + 1] , "value" : parseInt(val[val.length - 1])};})
-           stat = stat.sort((a, b) => {return b.value - a.value})
-           
-           document.getElementsByClassName("info-contrat")[0].innerHTML = ""
-           var h = 1
-           $(".info-contrat").append("<h1 class='donneetexte n" + h + "'>" + stat[0].name + "</h>")
-
-           for(var i = 1; i < 4; i++){
-			   if(stat[i].value < stat[i - 1].value){
-				 h++;
-			   }
-				$(".info-contrat").append("<h1 class='donneetexte n" + h + "'>" + stat[i].name + "</h>")
-		   }
-
-			//8 : DD / quel aspect le plus représenté / bubble -> camembert
-			var names = stats.Developpement_durable.values[0]
-			var count = stats.Developpement_durable.values[1]
-			
-			var stat = names.map((val, id) => {return {"name": names[id] , "value" : parseInt(count[id])};})
-			stat = stat.sort((a, b) => {return b.value - a.value})
-			$(".info-dd h1").text(stat[0].name)
+        .then(function (stats) {
+            var contrats = stats.Contrats.values.slice(1);
+            var stat = contrats.map(function(val, id) {
+                return {
+                    "name": stats.Contrats.labels[id + 1],
+                    "value": parseInt(val[val.length - 1])
+                };
         });
-	
+            stat = stat.sort(function(a, b) {return b.value - a.value});
+
+            document.getElementsByClassName("info-contrat")[0].innerHTML = "";
+            var h = 1
+            $(".info-contrat").append("<h1 class='donneetexte n" + h + "'>" + stat[0].name + "</h>");
+
+            for (var i = 1; i < 4; i++) {
+                if (stat[i].value < stat[i - 1].value) {
+                    h++;
+                }
+                $(".info-contrat").append("<h1 class='donneetexte n" + h + "'>" + stat[i].name + "</h>")
+            }
+
+            //8 : DD / quel aspect le plus représenté / bubble -> camembert
+            var names = stats.Developpement_durable.values[0]
+            var count = stats.Developpement_durable.values[1]
+
+            var stat2 = names.map(function(val, id) {return {"name": names[id], "value": parseInt(count[id])};
+        });
+            stat2 = stat.sort(function(a, b) {return b.value - a.value});
+            $(".info-dd h1").text(stat2[0].name)
+        });
+
     fetch("/capeb/data/" + d.properties.siren_epci + "/sunburst")
         .then(function (value) {
             return value.json();
@@ -157,11 +163,26 @@ function miniStats(regionStats, d) {
             console.log(error);
             return {};
         })
-        .then(function(jsonContrats){
+        .then(function (jsonContrats) {
             //$("svg#sunburst").remove();
             //sunBurst(jsonContrats);
         });
     //4 : embauche / métier qui embauche le plus/ double chart with rotation
+    fetch("/capeb/data/" + d.properties.siren_epci + "/recrutements")
+        .then(function (value) {
+            return value.json();
+        })
+        .catch(function (error) {
+            console.log("error");
+            console.log(error);
+            return {};
+        })
+        .then(function (json) {
+            var asNumbers = json.values[0].map(Number);
+            var indexOfMax = asNumbers.indexOf(Math.max(...asNumbers));
+            console.log(json.labels[indexOfMax]);
+            document.getElementsByClassName("info-emploi")[0].getElementsByClassName("donnee")[0].innerHTML = json.labels[indexOfMax];
+        });
 
     //5 : distance / moyenne / bubble chart
     fetch("/capeb/data/" + d.properties.siren_epci + "/distance")
