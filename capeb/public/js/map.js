e86136467a09c25b1f5408df728b7ca7fc44b028
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-02-04 21:33:58 +0100

    center and scale map

--- capeb/public/js/map.js
+++ capeb/public/js/map.js
@@ -1,3 +1,8 @@
+var viewportWidth = $(".map-pdl").width();
+var viewportHeight = $(".map-pdl").height();
+var width = viewportWidth;
+var height = viewportHeight;
+
 "use strict";
 $ (document).ready(function(){
     document.getElementsByClassName("backToMap")[0].addEventListener("click", function () {
@@ -5,29 +10,37 @@ $ (document).ready(function(){
         document.location.href = location.slice(0, location.lastIndexOf("/"));
     });
 });
-
-
-var viewportWidth = $(".map-pdl").width();
-var viewportHeight = $(".map-pdl").height();
-var width = viewportWidth;
-var height = viewportHeight;
-
-var page2 = d3.select(".map-pdl");
-var svg = page2.append("svg")
+// to use on resize
+var projection, scale2, offset2;
+var map = d3.select(".map-pdl");
+var svg = map.append("svg")
     .attr("width", width).attr("height", height);
 
-console.log(width);
-console.log(height);
-var projection = d3.geoConicConformal()
-    .center([2.8, 44.5])
-    .scale(width*10)
-    .translate([width, height]);
-var path = d3.geoPath(projection);
 
-path.projection(projection);
 
 $ (document).ready(function(){
     d3.json("data/merged.geojson", function (err, geoJSON) {
+        var center = d3.geo.centroid(geoJSON);
+        var scale = width*10;
+        var offset = [width/2, height/2];
+        projection = d3.geo.mercator().scale(scale).center(center).translate(offset);
+
+        var path = d3.geoPath(projection);
+
+        // using the path determine the bounds of the current map and use
+        // these to determine better values for the scale and translation
+        var bounds  = path.bounds(geoJSON);
+        var hscale  = scale*width  / (bounds[1][0] - bounds[0][0]);
+        var vscale  = scale*height / (bounds[1][1] - bounds[0][1]);
+        scale2   = (hscale < vscale) ? hscale : vscale;
+        offset2  = [width - (bounds[0][0] + bounds[1][0])/2, height - (bounds[0][1] + bounds[1][1])/2];
+
+        // new projection
+        projection = d3.geo.mercator().center(center)
+            .scale(scale2).translate(offset2);
+        path = path.projection(projection);
+
+
         var map = svg.selectAll("path").data(geoJSON.features);
         map.enter()
             .append("path")
@@ -76,14 +89,14 @@ $ (document).ready(function(){
 d3.select(window).on('resize', resize);
 
 function resize() {
-    var width = parseInt(d3.select('.map-pdl').style('width'));
-    var height = parseInt(d3.select('.map-pdl').style('heigth'));
+    var width = document.getElementsByClassName("map-pdl")[0].style.width;
+    var height = document.getElementsByClassName("map-pdl")[0].style.height;
 
     projection
-        .scale([width*10])
+        .scale(scale2)
         .translate([width/2,height/2]);
 
     d3.select("map-pdl").attr("width",width).attr("height",height);
     d3.select("svg").attr("width",width).attr("height",height);
-    d3.selectAll("path").attr('d', path);
+        d3.selectAll("path").attr('d', path);
 }
