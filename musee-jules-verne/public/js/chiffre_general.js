19b94263c1c228eab542f4bc6ad3249d69bfd893
Amaury <amaury.comte@etu.univ-nantes.fr>
2018-02-04 15:53:23 +0100

    mise a jour site

--- musee-jules-verne/public/js/chiffre_general.js
+++ musee-jules-verne/public/js/chiffre_general.js
@@ -1,3 +1,16 @@
+var params_jules_verne = {
+        container: document.getElementById('jules_verne'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        animationData: jules_verne,
+	name: 'julesverne',
+    };
+
+
+    var anim_jules_verne;
+
+
 function anim_chiffre1() {
 	anim_chiffre_1 = lottie.loadAnimation(params_chiffre_1);
 }
@@ -17,47 +30,65 @@ function anim_chiffre4() {
 function anim_chiffre5() {
 	anim_chiffre_5 = lottie.loadAnimation(params_chiffre_5);
 }
+
+function anim_jules_verne() {
+	lottie.play();
+}
 	  
-	  $(document).ready(function() {
-      $('#fullpage').fullpage({
-      navigation: true,
-          afterLoad: function(anchorLink, index){
-			  			  //Anim CSS arrière Plan class:visited
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        afterLoad: function(anchorLink, index){
+    	  			  //Anim CSS arrière Plan class:visited
         var currentSection = $('#fullpage').children().eq(index - 1);
         currentSection.addClass('visited');
-			  var loadedSection = $(this);
-			  
-			  //Anim chiffre section se lance à l'entrée
-		if(index === 3 && chiffre1_activ === 0){
-			  setTimeout(anim_chiffre1, 1000);
-			chiffre1_activ = 1;
-		}
-			  if(index === 4 && chiffre2_activ === 0){
-			  setTimeout(anim_chiffre2, 1000);
-				  chiffre2_activ = 2;
-			  }
-			  if(index === 5 && chiffre3_activ === 0){
-			  setTimeout(anim_chiffre3, 1000);
-				  chiffre3_activ = 3;
-			  }
-			  if(index === 6 && chiffre4_activ === 0){
-			  setTimeout(anim_chiffre4, 1000);
-				  chiffre4_activ = 4;
-			  }
-			  if(index === 7 && chiffre5_activ === 0){
-			  setTimeout(anim_chiffre5, 1000);
-				  chiffre5_activ = 5;
-			  }
-		}			  
-      
+    	var loadedSection = $(this);
+
+        //Anim Jules Verne section se lance à l'entrée
+
+        if(index === 2 && jules_verne_activ === 0){
+			
+            setTimeout(anim_jules_verne, 1000);
+			lottie.play();
+            jules_verne_activ = 1;
+        }
+    	  
+    	//Anim chiffre section se lance à l'entrée
+        if(index === 3 && chiffre1_activ === 0){
+    	   setTimeout(anim_chiffre1, 1000);
+    	   chiffre1_activ = 1;
+        }
+
+    	if(index === 4 && chiffre2_activ === 0){
+    	   setTimeout(anim_chiffre2, 1000);
+    	   chiffre2_activ = 2;
+    	}
+
+    	if(index === 5 && chiffre3_activ === 0){
+    	   setTimeout(anim_chiffre3, 1000);
+    		chiffre3_activ = 3;
+    	}
+    	  
+        if(index === 6 && chiffre4_activ === 0){
+    	   setTimeout(anim_chiffre4, 1000);
+    		chiffre4_activ = 4;
+    	  }
+
+    	if(index === 7 && chiffre5_activ === 0){
+    	   setTimeout(anim_chiffre5, 1000);
+    		chiffre5_activ = 5;
+    	}
+        }			  
+          
     });
-  });
+});
 
 var chiffre1_activ = 0;
 var chiffre2_activ = 0;
 var chiffre3_activ = 0;
 var chiffre4_activ = 0;
 var chiffre5_activ = 0;
+var jules_verne_activ = 0;
 
 var params_chiffre_1 = {
         container: document.getElementById('chiffre1'),
@@ -114,5 +145,18 @@ var params_chiffre_5 = {
 
  	var anim_chiffre_5;
 
+var params_jules_verne = {
+        container: document.getElementById('jules_verne'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        animationData: jules_verne,
+	name: 'jules_verne',
+    };
+
+
+    var anim_jules_verne;
+
+    anim_jules_verne = lottie.loadAnimation(params_jules_verne);
 
 	
--- musee-jules-verne/public/js/section6/graph1.js
+++ musee-jules-verne/public/js/section6/graph1.js
@@ -1,143 +1,86 @@
+//===== Graphique 1 de la slide 6
+// Note pour l'intégration: ce graphique est complètement responsive (normalement)
+//grâce à la déclaration [options: {  responsive: true, maintainAspectRatio: false,}] ligne 36.
 
-//================= Graphique 1 - Mois
+var ctx = document.getElementById('slide6_graphique1');
 
-var ctx = document.getElementById("slide3_graphique1").getContext('2d');
+var slide6_graphique1 = new Chart(ctx, {
+  type: 'horizontalBar',
+  data: {
+    labels: ['Divers', 'Arts de la scène', 'Littérature', 'Tourisme', 'Exposition', 'Audiovisuel', 'Sciences et Techniques', 'Animation', 'Conférence', 'Politique'],
+    datasets: [
+      {
+        label: 'Simple Mention de Jules Verne (%)',
+        data: [68.62,11.95,59.82,40.65,33.5,40.17,18.35,18.4,24.4,93.65],
+        backgroundColor: '#bde9ff',
+      },
+      {
+        label: "Inspiré par Jules Verne (%)",
+        data: [20.52,31.45,15.85,39.4,26.35,24.1,58.95,58.95,10.45,1.75],
+        backgroundColor: '#8ac0ff',
+      },
+      {
+        label: "Consacré Jules Verne (%)",
+        data: [10.56,3.3,19,19.75,39.6,10.88,20.55,20.55,65.15,4.6],
+        backgroundColor: '#0972ff',
+      },
+      {
+        label: "Adaptation d'une œuvre de Jules Verne (%)",
+        data: [0.3,53.3,5.33,0.2,0.55,24.85,2.15,2.1,0,0],
+        backgroundColor: '#0c5dc1',
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
 
-  //Définition du dégradé jaune
-  var yellow_gradient = ctx.createLinearGradient(0, 0, 0, 600);
-  yellow_gradient.addColorStop(0, 'rgba(255,184,0,1)');
-  yellow_gradient.addColorStop(0.9, 'rgba(255,184,0,0)');
-  yellow_gradient.addColorStop(1, 'rgba(255,184,0,0)');
+    tooltips: {//personnalistion des bulles d'informations
+      enabled: true,
+        cornerRadius: 4,
+        caretSize: 0,//taille de la flèche
+        xPadding: 16,
+        yPadding: 16,
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        titleFontStyle: 'bold',//gestion du titre
+        titleMarginBottom: 15,
+        titleFontColor: 'black',
+        bodyFontColor: 'black',//gestion du texte
+        multiKeyBackground: 'white',
+      },
 
-  //Définition du dégradé jaune au survol
-  var hover_yellow_gradient = ctx.createLinearGradient(0, 0, 0, 600);
-  hover_yellow_gradient.addColorStop(0, 'rgba(255,184,0,1)');
-  hover_yellow_gradient.addColorStop(0.5, 'rgba(255,184,0,0.75)');
-  hover_yellow_gradient.addColorStop(1, 'rgba(255,184,0,0)');
+    title: {
+      display: true,
+      text: "Nature de la référence à Jules Verne, selon le sujet de l'article",
+      position: 'bottom',
+    },
 
-  var slide3_graphique1 = new Chart(ctx, {
-    type: 'bar',
-data: {
-  datasets: [{
-        label: 'Janvier',
-        data: ["Non indiqué", 179, 128, 218, 112, 328, 123, 524, 253, 396, 430],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      }, {
-        label: 'Février',
-        data: ["Non indiqué", 173, 111, 361, 230, 396, 316, 358, 397, 496, 534],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Mars',
-        data: ["Non indiqué", 149, 158, 211, 339, 289, 330, 455, 391, 401, 676],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Avril',
-        data: ["Non indiqué", 153, 14, 325, 274, 355, 397, 480, 30, 399, 320],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Mai',
-        data: ["Non indiqué", 123, 125, 285, 532, 380, 380, 479, 51, 394, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Juin',
-        data: ["Non indiqué", 148, 135, 293, 226, 379, 245, 423, 208, 310, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Juillet',
-        data: [65, 323, 364, 240, 238, 415, 378, 333, 427, 432, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Août',
-        data: [198, 131, 271, 209, 183, 297, 343, 294, 317, 240, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Septembre',
-        data: [107, 121, 173, 243, 316, 231, 347, 396, 379, 344, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Octobre',
-        data: [192, 153, 197, 282, 383, 456, 398, 570, 502, 333, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Novembre',
-        data: [181, 147, 300, 232, 314, 395, 431, 519, 527, 410, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      },{
-        label: 'Décembre',
-        data: [261, 106, 243, 133, 200, 174, 391, 466, 475, 676, "Non indiqué"],
-        backgroundColor: yellow_gradient,
-        hoverBackgroundColor: hover_yellow_gradient,
-      }, {
-        label: 'Articles publiés par année',
-        data: [1004,1906,2219,3032,3347,4095,4079,5297,3957,4831,1970],
-        fill: false,
-        borderColor: "#0972FF",
-        backgroundColor: "#0972FF",
-        type: 'line',
-        pointBackgroundColor: "rgba(255,255,255,1)",
-        pointRadius: 4,
-        pointHoverRadius: 8,
-        pointHoverBackgroundColor: "rgba(255,255,255,1)",
-        pointHoverBorderColor: "#0972FF",
+    legend: {
+    display: false,
+    },
+
+    scales: {
+      yAxes: [{
+        stacked: true,
+        categoryPercentage: 1.0,
+        barPercentage: 0.95, // ces 2 propriétés retirent tout espace en les barres
+          gridLines: {
+            display: false,
+          },
       }],
-  labels: ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
-},
-      options: {
-        responsive: true,
-         //maintainAspectRatio: false,
+      xAxes: [{
 
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true,
-                      //max:5500, //empêche l'échelle Y de changer / se resizer
-                  },
-                  gridLines: {
-                    lineWidth: 0.5,
-                    color: "rgba(0,0,0,0.2)",
-                    borderDash: [2,4],
-                    zeroLineColor: "rgba(0,0,0,0)",
-                  },
-              }],
-              xAxes: [{
-                 categoryPercentage: 1.0,
-                  barPercentage: 1, // ces 2 propriétés retirent tout espace en les barres
-                  gridLines: {
-                    lineWidth: 0.5,
-                    color: "rgba(0,0,0,0.5)",
-                    borderDash: [2,4],
-                    zeroLineColor: "rgba(0,0,0,1)",
-                  },
-              }]
+        stacked: true,
+          gridLines: {
+            display: false,
           },
-          legend: {
-          labels: {
-               filter: function(legendItem, chartData) {
-                if (legendItem.datasetIndex === 12) {//fait apparaitre la légende de la 12e data (en l'occurence, le nombre d'articles publiés par années)
-                  return true;
-                }
-               return false;
-             },
-               fontColor: "#0972FF",
-
+          ticks: {
+            max:100, //Force l'échelle à s'arrêter à 100
+          },
+      }],
     }
-          },
-          title: {
-            position: 'bottom',
-            display: true,
-            text: "Nombre d'articles publiés sur Jules Verne, de juillet 2007 à avril 2017" ,
-          },
-        },
+  }
 
-  });
+});
+
+//===== Fin du Graphique 1 de la slide 6
