e4ec008fac4c818f96770fface1e89033d2d3077
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-02-05 22:09:54 +0100

    bug fix

--- capeb/public/js/charts.js
+++ capeb/public/js/charts.js
@@ -535,33 +535,5 @@ function createConjonctureDataviz(json){
             }
         }
     });
-
-    Chart.plugins.register({
-        afterDatasetsDraw: function(chartInstance, easing) {
-            // To only draw at the end of animation, check for easing === 1
-            var ctx = chartInstance.chart.ctx;
-            chartInstance.data.datasets.forEach(function (dataset, i) {
-                var meta = chartInstance.getDatasetMeta(i);
-                if (!meta.hidden) {
-                    meta.data.forEach(function(element, index) {
-                        // Draw the text in black, with the specified font
-                        ctx.fillStyle = 'rgb(0, 0, 0)';
-                        var fontSize = 16;
-                        var fontStyle = 'normal';
-                        var fontFamily = 'Helvetica Neue';
-                        ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-                        // <---- ADJUST TO DESIRED TEXT --->
-                        var dataString = dataset.data[index].text;
-                        // Make sure alignment settings are correct
-                        ctx.textAlign = 'center';
-                        ctx.textBaseline = 'middle';
-                        var padding = 5;
-                        var position = element.tooltipPosition();
-                        ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
-                    });
-                }
-            });
-        }
-    });
 }
 
