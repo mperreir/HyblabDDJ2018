a9f7440d1de9372d6abdd491a6d329c8507b29a3
Eric Lentz <lentz.eric.b.p@gmail.com>
2018-02-06 18:27:00 +0100

    oups

--- france3-pdl/server.js
+++ france3-pdl/server.js
@@ -17,5 +17,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 // server and visiting http(s)://127.0.0.1:8080/name_of_you_project/ (if on a local server)
 // or more generally: http(s)://server_name:port/name_of_you_project/
 module.exports = app;
-
-app.listen(8080);
